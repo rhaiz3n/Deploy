59adad9f92550b0156c893a18a737b7019b1704f
rhaiz3n <119270733+rhaiz3n@users.noreply.github.com>
2022-12-14 21:11:42 +0800

    Update features.jsx

--- components/features.jsx
+++ components/features.jsx
@@ -4,16 +4,16 @@ const Features  = () => {
     return (
         <div className={styles.container}>
             <div className={styles.boxes}>
-                <image src="globe.png" className={styles.icons} />
+                <img src="globe.png" className={styles.icons} />
                 <p>Browse thousands of research and thesis papers.</p>
             </div>
             <div className={styles.boxes}>
-                <image src="upload-download.png" className={styles.icons} />
+                <img src="upload-download.png" className={styles.icons} />
                 <p>Upload your work or download anything that you like.</p>
 
             </div>
             <div className={styles.boxes}>
-                <image src="navigation.png" className={styles.icons} />
+                <img src="navigation.png" className={styles.icons} />
                 <p>Navigate to your own preference to the user friendly</p>
             </div>
         </div>
