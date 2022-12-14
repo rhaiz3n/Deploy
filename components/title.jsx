98f6c06337fccf1765202ef018fc4210566568f1
rhaiz3n <119270733+rhaiz3n@users.noreply.github.com>
2022-12-14 21:13:12 +0800

    Update title.jsx

--- components/title.jsx
+++ components/title.jsx
@@ -7,7 +7,7 @@ return (
     
     <div className={styles.container}>
         <div className={styles.background}>
-            <image src="home.png" alt="Background Image - Bulb" className={styles.home_img} />
+            <img src="home.png" alt="Background Image - Bulb" className={styles.home_img} />
             
             <div className={styles.text}>
                 <div className={styles.title1}>
@@ -16,7 +16,7 @@ return (
                 <p>Search thousands of research and thesis papers.</p>
                 <form className={styles.search_box}>
                     <input className={styles.search} type="text" name="search"/>
-                    <image src="search_icon.png" className={styles.search_icon} />
+                    <img src="search_icon.png" className={styles.search_icon} />
                 </form>
             </div>
             
